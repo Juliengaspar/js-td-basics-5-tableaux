59a862014fb827170378fbeac1c78e6edd3c7662
Juliengaspar <julien.gaspar@student.hepl.be>
2025-10-22 13:45:01 +0200

    Update script022-1-Array.js
    first init

--- starter/script022-1-Array.js
+++ starter/script022-1-Array.js
@@ -4,14 +4,16 @@
 
 // 1. Initialiser un nouveau tableau
 // EXERCICE 1 : Créez un tableau appelé "semaine" qui contient les noms des jours de la semaine
-
+const semaine = ['lundi','mardi','mercredi','jeudi','vendredi', 'samedi', 'dimanche']
 
 // 2. Accéder à un élément du tableau
 // EXERCICE 2 : Affichez le premier jour de la semaine et le dernier jour de la semaine en utilisant les indices du tableau
-
+console.log(semaine[0])//affichier le prmeeir elements du tableaux grace a l'indice0
+console.log(semaine[6])//affichier le dernier elements du tableaux grace a l'indice6
+console.log(semaine[semaine.length-1])//affichier le dernier elements du tableaux grace a l'indice6
 // 3. Nombre d'éléments dans un tableau
 // EXERCICE 3 : Affichez le nombre de jours dans une semaine et le dernier jour de la semaine
-
+console.log(semaine.length);
 
 // 4. Modifier les données d'un tableau
 // EXERCICE 4 : Modifiez "samedi" en "saturday", réaffichez le tableau, stockez "dimanche" dans une variable jourFerie, affichez le contenu de la variable, ajoutez un nouveau jour, "dimanche2", à la fin de la semaine, puis réaffichez le tableau des jours de la semaine.
